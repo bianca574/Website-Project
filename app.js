17dd90402975fcaced98ba263b88875fce20d25d
bianca574 <biancailieserban@yahoo.com>
2025-06-24 22:04:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+document.querySelectorAll(".toggle-btn").forEach(button => {
+    
+    button.addEventListener("click", () => {
+        
+        const container = button.closest(".section-content") || button.closest(".content-section");
+
+        if(!container) return;
+
+        container.classList.toggle("expanded");
+        
+        button.textContent = container.classList.contains("expanded") ? "Show Less" : "Learn More";
+    });
+
+});  
